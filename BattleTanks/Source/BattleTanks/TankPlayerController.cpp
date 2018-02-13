4b877577b958b88688e70d5be26d5ec74f1a43df
Travis <crazyxtempiz@gmail.com>
2018-02-12 19:50:08 -0500

    Added MainMenu

---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "Battletanks.h"
+
+/*ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+*/
+
